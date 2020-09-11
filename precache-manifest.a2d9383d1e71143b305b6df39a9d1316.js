5b4171294f46873444b7255bd34e0cf96215a281
khudnytskaya <54635860+khudnytskaya@users.noreply.github.com>
2020-09-11 15:22:48 +0300

    Updates

---
+++ precache-manifest.a2d9383d1e71143b305b6df39a9d1316.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf7a765b55c1e1a259810d913a7d520",
+    "url": "/text-editor/index.html"
+  },
+  {
+    "revision": "2e686310f3526745540a",
+    "url": "/text-editor/static/css/2.7db60f5a.chunk.css"
+  },
+  {
+    "revision": "8a2d7326b6cbe4faccf3",
+    "url": "/text-editor/static/css/main.9e53bee7.chunk.css"
+  },
+  {
+    "revision": "2e686310f3526745540a",
+    "url": "/text-editor/static/js/2.212f7957.chunk.js"
+  },
+  {
+    "revision": "ae69a686e1f284d78b6ec2bbf4577426",
+    "url": "/text-editor/static/js/2.212f7957.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a2d7326b6cbe4faccf3",
+    "url": "/text-editor/static/js/main.925272ea.chunk.js"
+  },
+  {
+    "revision": "471cc5c3d951615acfc3",
+    "url": "/text-editor/static/js/runtime-main.a21fc011.js"
+  }
+]);
